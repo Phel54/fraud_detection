3ffbffc9e408a85877cb2d670aeb6c0e9ca254c7
Isichei Phelim <isicheiphelim@gmail.com>
2022-01-20 21:15:47 +0000

    deleted ts files

--- src/app/users/users.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import usersController from './users.controller';
-const userRouter = express.Router();
-
-userRouter.route('/create').post(usersController.createUser);
-userRouter.route('/login').post(usersController.login);
-
-export default userRouter;
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv';
-import app from './server';
-import { database } from './config/database';
-const PORT = 3000;
-
-app.listen(PORT, async () => {
-    await database()
-    console.log(`listening on: http://localhost:${PORT}`);
-})
